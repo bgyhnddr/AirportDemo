9d16020961828a7237e22cc3b9a9322c833b6018
bgyhnddr <bgyhnddr@163.com>
2014-06-13 02:15:35 +0800

    first commit
    123

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace AirportDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.img = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Flight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TOVIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Counter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.STD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Remark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(199)))), ((int)(((byte)(201)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.img,
+            this.Flight,
+            this.TOVIA,
+            this.Counter,
+            this.STD,
+            this.Gate,
+            this.Remark});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle4.Padding = new System.Windows.Forms.Padding(0, 10, 10, 0);
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowTemplate.Height = 52;
+            this.dataGridView1.Size = new System.Drawing.Size(1366, 768);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            // 
+            // img
+            // 
+            this.img.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.img.DataPropertyName = "img";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle2.NullValue")));
+            this.img.DefaultCellStyle = dataGridViewCellStyle2;
+            this.img.FillWeight = 1F;
+            this.img.HeaderText = "";
+            this.img.Name = "img";
+            this.img.ReadOnly = true;
+            // 
+            // Flight
+            // 
+            this.Flight.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Flight.DataPropertyName = "Flight";
+            this.Flight.FillWeight = 3F;
+            this.Flight.HeaderText = "航班号\nFLIGHT";
+            this.Flight.Name = "Flight";
+            this.Flight.ReadOnly = true;
+            // 
+            // TOVIA
+            // 
+            this.TOVIA.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TOVIA.DataPropertyName = "TOVIA";
+            this.TOVIA.FillWeight = 6F;
+            this.TOVIA.HeaderText = "终点站/经停\nTO/VIA";
+            this.TOVIA.Name = "TOVIA";
+            this.TOVIA.ReadOnly = true;
+            // 
+            // Counter
+            // 
+            this.Counter.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Counter.DataPropertyName = "Counter";
+            this.Counter.FillWeight = 4F;
+            this.Counter.HeaderText = "值机柜台\nCOUNTER";
+            this.Counter.Name = "Counter";
+            this.Counter.ReadOnly = true;
+            // 
+            // STD
+            // 
+            this.STD.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.STD.DataPropertyName = "STD";
+            this.STD.FillWeight = 4F;
+            this.STD.HeaderText = "起飞时间\nSTD";
+            this.STD.Name = "STD";
+            this.STD.ReadOnly = true;
+            // 
+            // Gate
+            // 
+            this.Gate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Gate.DataPropertyName = "Gate";
+            this.Gate.FillWeight = 3F;
+            this.Gate.HeaderText = "登机口\nGATE";
+            this.Gate.Name = "Gate";
+            this.Gate.ReadOnly = true;
+            // 
+            // Remark
+            // 
+            this.Remark.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Remark.DataPropertyName = "Remark";
+            this.Remark.FillWeight = 8F;
+            this.Remark.HeaderText = "备注\nREMAKE";
+            this.Remark.Name = "Remark";
+            this.Remark.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewImageColumn img;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Flight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TOVIA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Counter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Remark;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AirportDemo
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<msg> list = new BindingList<msg>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var row = new msg();
+            row.img = Image.FromFile("H:\\Airport\\AirportDemo\\icon1.png");
+            row.Flight = "CZ3915";
+            row.TOVIA = "海口";
+            row.Counter = "52-53";
+            row.STD = "11:30";
+            row.Gate = "6";
+            row.Remark = "正在办理CHECK-IN";
+
+            var row2 = new msg();
+            row2.img = Image.FromFile("H:\\Airport\\AirportDemo\\icon1.png");
+            row2.Flight = "CZ1234";
+            row2.TOVIA = "北京";
+            row2.Counter = "52-53";
+            row2.STD = "11:30";
+            row2.Gate = "6";
+            row2.Remark = "正在办理CHECK-IN";
+            list.Add(row);
+            list.Add(row2);
+            dataGridView1.DataSource = list;
+            
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                Rectangle newRect = new Rectangle(e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width, e.CellBounds.Height);              
+                Brush backColorBrush = new SolidBrush(e.CellStyle.BackColor);//非选中的背景色
+                e.Graphics.FillRectangle(backColorBrush, e.CellBounds);//用背景色填充单元格
+
+
+                if (e.ColumnIndex == 0)
+                {
+                    ControlPaint.DrawBorder(e.Graphics, newRect,
+                    Color.FromArgb(40, 40, 40), 0, ButtonBorderStyle.None,
+                    Color.FromArgb(40, 40, 40), 10, ButtonBorderStyle.Solid,
+                    Color.FromArgb(40, 40, 40), 0, ButtonBorderStyle.None,
+                    Color.FromArgb(40, 40, 40), 0, ButtonBorderStyle.None);
+                }
+
+                else
+                {
+                    ControlPaint.DrawBorder(e.Graphics, newRect,
+                        Color.FromArgb(40, 40, 40), 0, ButtonBorderStyle.None,
+                        Color.FromArgb(40, 40, 40), 10, ButtonBorderStyle.Solid,
+                        Color.FromArgb(40, 40, 40), 10, ButtonBorderStyle.Solid,
+                        Color.FromArgb(40, 40, 40), 0, ButtonBorderStyle.None);
+                }
+
+
+
+
+                if (e.Value != null)
+                {
+                    if (e.ColumnIndex == 0)
+                    {
+                        newRect.Offset(100, 100);
+
+                        Rectangle newRect2 = new Rectangle(0, 0, 100, 100);
+                        e.PaintContent(newRect2);//画内容
+                    }
+                    e.PaintContent(newRect);//画内容
+                }
+                e.Handled = true;
+            }
+        }
+
+
+       
+    }
+
+    public class msg
+    {
+        public Image img { get; set; }
+        public string Flight { get; set; }
+        public string TOVIA { get; set; }
+        public string Counter { get; set; }
+        public string STD { get; set; }
+        public string Gate { get; set; }
+        public string Remark { get; set; }
+       
+    }
+}
